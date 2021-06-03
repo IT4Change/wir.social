9bfc8e8753f7297f91b868c64e804a5738c46afd
Wolfgang Huß <wolle.huss@pjannto.com>
2021-06-03 07:54:47 +0200

    Extend and change links

--- branding/constants/links.js
+++ branding/constants/links.js
@@ -1,8 +1,13 @@
 // this file is duplicated in `backend/src/config/links.js` and `webapp/constants/links.js` and replaced on rebranding
 export default {
   ORGANIZATION: 'https://webcraft-media.de',
+  SUPPORT: 'https://webcraft-media.de',
+
+  // on null or empty string internal imprint is used, see 'webapp/locales/html/'
   DONATE: 'https://webcraft-media.de/donate-for-wir-social.html',
   IMPRINT: 'https://www.webcraft-media.de/#!impressum',
   DATA_PRIVACY: 'https://www.webcraft-media.de/#!datenschutz',
+  TERMS_AND_CONDITIONS: null,
+  CODE_OF_CONDUCT: null,
   FAQ: 'https://www.webcraft-media.de/#!contact',
 }
