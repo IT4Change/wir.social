862218a2dc75359ec89ae114c8dcf38828a302c0
Wolfgang Huß <wolle.huss@pjannto.com>
2021-06-07 21:26:50 +0200

    Correct 'metadata.js'

--- branding/constants/metadata.js
+++ branding/constants/metadata.js
@@ -1,7 +1,7 @@
 // this file is duplicated in `backend/src/config/metadata.js` and `webapp/constants/metadata.js` and replaced on rebranding
 export default {
   APPLICATION_NAME: 'wir.social',
-  APPLICATION_SHORT_NAME: 'wir',
+  APPLICATION_SHORT_NAME: 'wir.social',
   APPLICATION_DESCRIPTION: 'Wir Social Community',
   COOKIE_NAME: 'ocelot-social-token',
   ORGANIZATION_NAME: 'Webcraft-Media',
