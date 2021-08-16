8efa08eccd0d18b859b6c973d879a2a73fc5fd16
Wolfgang Huß <wolle.huss@pjannto.com>
2021-08-16 15:30:07 +0200

    Change organization and support link in backend for now

--- branding/constants/emails.js
+++ branding/constants/emails.js
@@ -3,6 +3,6 @@ export default {
   SUPPORT_EMAIL: 'team@wir.social',
   MODERATION_EMAIL: 'team@wir.social',
   // ATTENTION: the following links have to be defined even for internal pages with full URLs as example like 'https://staging.ocelot.social/support', because they are used in e-mails!
-  ORGANIZATION_LINK: 'https://wir.social/organization',
-  SUPPORT_LINK: 'https://wir.social/support',
+  ORGANIZATION_LINK: 'https://webcraft-media.de', // Wolle 'https://wir.social/organization',
+  SUPPORT_LINK: 'https://webcraft-media.de', // Wolle 'https://wir.social/support',
 }
